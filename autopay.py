4a3a4ac7f846cf06dd03b5318521c0ff434c36f4
Yashwant Singh Waskel <waskel.1@iitj.ac.in>
2021-06-12 12:50:01 +0530

    add autopay.py

--- autopay.py
+++ autopay.py
@@ -1,34 +1,90 @@
-import unittest
 from appium import webdriver
-from time import sleep
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from appium.webdriver.common.touch_action import TouchAction
+import time
+mname = "xx"
+# input("Enter the middle name u want to set :")
 
-desired_capabilities = {
-  "deviceName": "vivo 1802",
-  "platformName": "android",
-  "appPackage": "com.enstage.wibmo.hdfc",
-  "appActivity": "com.enstage.wibmo.main.MainActivity",
-  "noReset": True }
+dcs = []
+dc = {}
+dc['platformName'] = 'Android'
+dc['deviceName'] = '16824257'
+dc['noReset'] = 'true'
+dc['appPackage'] = 'com.enstage.wibmo.hdfc'
+dc['appActivity'] = 'com.enstage.wibmo.main.MainActivity'
+dc['realDevice'] = 'true'
+dcs.append(dc)
 
-driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_capabilities)
-# wait = WebDriverWait(driver, 30);
-# el = wait.until(EC.element_to_be_clickable((By.XPATH, "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TableLayout/android.widget.TableRow[2]/android.widget.Button[2]")))
-# el.click()
-driver.implicitly_wait(30)
-key1 = driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TableLayout/android.widget.TableRow[2]/android.widget.Button[2]").click()
-key2 = driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TableLayout/android.widget.TableRow[1]/android.widget.Button[1]").click()
-key3 = driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TableLayout/android.widget.TableRow[1]/android.widget.Button[2]").click()
-key4 = driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TableLayout/android.widget.TableRow[2]/android.widget.Button[3]").click()
+dc2={}
+dc2['platformName'] = 'Android'
+dc2['deviceName'] = '16824257'
+dc2['noReset'] = 'true'
+dc2['appPackage'] = 'com.excean.parallelspace'
+dc2['appActivity'] = 'com.excelliance.kxqp.ui.MainActivity'
+dc2['realDevice'] = 'true'
+dcs.append(dc2)
 
-login = driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TableLayout/android.widget.TableRow[5]/android.widget.Button").click()
-driver.implicitly_wait(20)
-skip = driver.find_element_by_id("com.enstage.wibmo.hdfc:id/buttonNegative").click()
+# dc['isHeadless'] = 'true'
+for i in [0,1]:
+    dr = None
+    if i == 0:
+        dr = webdriver.Remote('http://localhost:4723/wd/hub', dcs[0])
+    else :
+        dr = webdriver.Remote('http://localhost:4723/wd/hub', dcs[1])
+        time.sleep(1)
+    if i == 1:
+        el1 = dr.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.ScrollView/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.GridView/android.widget.LinearLayout[1]/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ImageView[2]")
+        el1.click()
 
-# search_button = driver.find_element_by_id("com.enstage.wibmo.hdfc")
-# search_button.click()
+    time.sleep(4)
+    el1 = dr.find_element_by_xpath(
+        "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TableLayout/android.widget.TableRow[1]/android.widget.Button[1]")
 
-# name_search_box = driver.find_element_by_id("com.whatsapp:id/search_input")
-# name_search_box.send_keys("chamma")
+    el2 = dr.find_element_by_xpath(
+        "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TableLayout/android.widget.TableRow[1]/android.widget.Button[2]")
+
+    el3 = dr.find_element_by_xpath(
+        "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TableLayout/android.widget.TableRow[1]/android.widget.Button[3]")
+
+    el4 = dr.find_element_by_xpath(
+        "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TableLayout/android.widget.TableRow[2]/android.widget.Button[1]")
+
+    el5 = dr.find_element_by_xpath(
+        "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TableLayout/android.widget.TableRow[2]/android.widget.Button[2]")
+
+    el6 = dr.find_element_by_xpath(
+        "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TableLayout/android.widget.TableRow[2]/android.widget.Button[3]")
+
+    if i == 0:
+        el1.click()
+        el2.click()
+        el3.click()
+        el4.click()
+        el5.click()
+        el6.click()
+    if i == 1:
+        el5.click()
+        el1.click()
+        el2.click()
+        el6.click()
+
+    el6 = dr.find_element_by_id("com.enstage.wibmo.hdfc:id/login_button")
+    el6.click()
+    time.sleep(2.25)
+    el2 = dr.find_element_by_id("com.enstage.wibmo.hdfc:id/buttonNegative")
+    el2.click()
+    el3 = dr.find_element_by_accessibility_id("Navigate up")
+    el3.click()
+    time.sleep(1)
+    TouchAction(dr).tap(x=275, y=1554).perform()
+
+    time.sleep(1)
+    el5 = dr.find_element_by_id("com.enstage.wibmo.hdfc:id/button_manageprofile")
+    el5.click()
+    time.sleep(0.5)
+    el6 = dr.find_element_by_id("com.enstage.wibmo.hdfc:id/main_mname_edit")
+    el6.clear()
+    el6.send_keys(mname)
+    el7 = dr.find_element_by_id("com.enstage.wibmo.hdfc:id/main_btnSave")
+    el7.click()
+    el8 = dr.find_element_by_accessibility_id("Navigate up")
+    el8.click()
