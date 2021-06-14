4bff7546e583a582f0ec9a29f95a3f2e83b10996
Yashwant Singh Waskel <waskel.1@iitj.ac.in>
2021-06-14 17:42:56 +0530

    done some changes

--- autopay.py
+++ autopay.py
@@ -1,6 +1,8 @@
 from appium import webdriver
 from appium.webdriver.common.touch_action import TouchAction
 import time
+import pandas as pd
+
 mname = "xx"
 # input("Enter the middle name u want to set :")
 
@@ -9,19 +11,35 @@ dc = {}
 dc['platformName'] = 'android'
 dc['deviceName'] = 'vivo 1802'
 dc['noReset'] = 'true'
-dc['appPackage'] = 'com.enstage.wibmo.hdfc'
-dc['appActivity'] = 'com.enstage.wibmo.main.MainActivity'
+dc['appPackage'] = 'multi.parallel.dualspace.cloner'
+dc['appActivity'] = 'multi.parallel.dualspace.cloner.components.ui.MainActivity'
 dc['realDevice'] = 'true'
 dcs.append(dc)
 
-dc2={}
-dc2['platformName'] = 'android'
-dc2['deviceName'] = 'vivo 1802'
-dc2['noReset'] = 'true'
-dc2['appPackage'] = 'com.excean.parallelspace'
-dc2['appActivity'] = 'com.excelliance.kxqp.ui.MainActivity'
-dc2['realDevice'] = 'true'
-dcs.append(dc2)
+# dc2={}
+# dc2['platformName'] = 'android'
+# dc2['deviceName'] = 'vivo 1802'
+# dc2['noReset'] = 'true'
+# dc2['appPackage'] = 'multi.parallel.dualspace.cloner'
+# dc2['appActivity'] = 'com.excelliance.kxqp.ui.MainActivity'
+# dc2['realDevice'] = 'true'
+# dcs.append(dc2)
+
+# dc3={}
+# dc3['platformName'] = 'android'
+# dc3['deviceName'] = 'vivo 1802'
+# dc3['noReset'] = 'true'
+# dc3['appPackage'] = 'multi.parallel.dualspace.cloner'
+# dc3['appActivity'] = 'com.excelliance.kxqp.ui.MainActivity'
+# dc3['realDevice'] = 'true'
+# dcs.append(dc3)
+
+passwords = ['123456', '5126','123456']
+# pas['abhishek']='123456'
+# pas['yashwant']='5126'
+# pas['sonu']='123456'
+# passwords.append(pas)
+
 
 numbers={"1": "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TableLayout/android.widget.TableRow[1]/android.widget.Button[1]",
          "2": "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TableLayout/android.widget.TableRow[1]/android.widget.Button[2]",
@@ -34,26 +52,29 @@ numbers={"1": "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout
          "9": "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TableLayout/android.widget.TableRow[3]/android.widget.Button[3]",
          "0": "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TableLayout/android.widget.TableRow[4]/android.widget.Button[2]"}
 
-password = {}
-password['abhishek']='123456'
-password['yashwant']='5126'
+apps = ["/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.RelativeLayout/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[1]/android.widget.RelativeLayout/android.widget.ImageView",
+        "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.RelativeLayout/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[2]/android.widget.RelativeLayout/android.widget.ImageView[1]",
+        "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.RelativeLayout/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[2]/android.widget.RelativeLayout/android.widget.ImageView[2]"]
 
-for i in [0,1]:
+app_index=0
+
+data = pd.read_excel (r'01_04-05-21_40355_61_JDP.xlsx') 
+df = pd.DataFrame(data, columns= ['Description', 'K Number'])
+length = len(df.index)+1
+
+i=0
+
+for k in range(0,length):
+	description = df.iloc[k]['Description']
+	k_num = df.iloc[k]['K Number']
     dr = None
     passw = None
-    if i == 0:
-        dr = webdriver.Remote('http://localhost:4723/wd/hub', dcs[0])
-        passw = password['abhishek']
-    else :
-        dr = webdriver.Remote('http://localhost:4723/wd/hub', dcs[1])
-        passw = password['yashwant']
-        time.sleep(1)
+    dr = webdriver.Remote('http://localhost:4723/wd/hub', dcs[0])
+    passw = passwords[i]
 
-    if i == 1:
-        el1 = dr.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.ScrollView/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.GridView/android.widget.LinearLayout[1]/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ImageView[2]")
-        el1.click()
+    el1 = dr.find_element_by_xpath(apps[i])
+    el1.click()
 
-    # time.sleep(4)
     dr.implicitly_wait(30)
 
     for j in passw:
@@ -65,8 +86,11 @@ for i in [0,1]:
 
     dr.implicitly_wait(30)
 
-    el2 = dr.find_element_by_id("com.enstage.wibmo.hdfc:id/buttonNegative")
-    el2.click()
+    try:
+    	el2 = dr.find_element_by_id("com.enstage.wibmo.hdfc:id/buttonNegative")
+    	el2.click()
+    except:
+    	pass
 
     bill_pay = dr.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[3]/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[5]/android.widget.RelativeLayout/android.widget.ImageView")
     bill_pay.click()
@@ -80,8 +104,26 @@ for i in [0,1]:
 
     dist_name = dr.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View[2]/android.view.View/android.widget.EditText")
     dist_name.click()
-    dist_name.send_keys("Adani Electricity Mumbai Limited")
+    dist_name.send_keys("Jodhpur vidyut vitran nigam")
+    dist_name.click()
     
-    to = dr.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View[2]/android.view.View[2]/android.widget.ListView/android.view.View[1]/android.view.View[2]")
-    to.click()
     time.sleep(5)
+
+    operator = dr.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View[2]/android.view.View[2]/android.widget.ListView/android.view.View[1]/android.view.View[2]")
+    operator.click()
+
+    k_num = "330126014901"
+    k_num_input = dr.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View[4]/android.view.View[2]/android.widget.EditText")
+    k_num_input.send_keys(k_num)
+
+    confirm = dr.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View[4]/android.view.View[3]/android.widget.Button")
+    confirm.click()
+
+    dr.implicitly_wait(30)
+
+    pay_now = dr.find_element_by_xpath ("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View[2]/android.view.View[7]/android.widget.Button")
+    time.sleep(5)
+
+    i=i+1
+    if i==3:
+    	i=0
